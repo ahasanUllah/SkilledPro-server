c0a4d77c90132f0b17ddd0db623d4d6c244abf68
ahasanUllah <bulbulahasan@gmail.com>
2022-10-25 21:01:02 +0600

    course data loaded

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors())
+
+const course = require('./Data/course.json')
 
 app.get('/', (req, res) => {
     res.send('Hello express')
+})
+
+app.get('/course', (req, res) => {
+    res.send(course)
+   
+})
+
+app.get('/course/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const single_course = course.find(c => c.id === id);
+    res.send(single_course);
+    console.log(typeof id);
 })
 
 app.listen(port, () => {
